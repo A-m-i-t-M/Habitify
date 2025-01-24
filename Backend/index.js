7248a79916b55a450d22df32b71670d4bf612acf
ox1g3n <aagnikg2003@gmail.com>
2025-01-24 12:42:57 +0530

    first backend commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,33 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
-const app = express();
+dotenv.config();
 
-app.listen(3000, ()=>{
-    console.log("Server is running");
-    
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to the Database...');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+  const app = express();
+
+  // Middleware
+  app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies and other credentials
+  }));
+  app.use(express.json());
+  app.use(cookieParser());
+  
+  // Start server
+  app.listen(3000, () => {
+    console.log('Server is up & running...');
+  });
+  

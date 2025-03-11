f28649b01d261fd71eeb3282a9b87768fb0b95aa
A-m-i-t-M <amitnoreply63@gmail.com>
2025-03-11 20:59:55 +0530

    CSS changes in Proflie page

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -73,7 +73,7 @@ export default function Profile() {
   
   return <div className='bg-gray-900 flex items-center justify-center min-h-screen bg-cover bg-center px-4'>
   <div className='bg-gray-900 text-white p-8 rounded-xl shadow-lg w-full max-w-md border border-white'>
-      <h1 className='text-3xl font-bold text-center mb-6 text-green-500'> </h1>
+      <h1 className='text-3xl font-bold text-center mb-6 text-green-500'> Update Profile </h1>
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
           <input 
               placeholder= "Username" 

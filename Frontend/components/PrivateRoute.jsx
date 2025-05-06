88da65236ce0e56ba663162acb963dcc869708a1
A-m-i-t-M <amitnoreply63@gmail.com>
2025-05-06 20:13:42 +0530

    asking me to commit before revert

--- Frontend/components/PrivateRoute.jsx
+++ Frontend/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
-import { useSelector } from '-redux'
-import { Navigate, Outlet } from '-router-dom';
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom';
 
 export default function PrivateRoute() {
     const {currentUser} = useSelector(state => state.user);

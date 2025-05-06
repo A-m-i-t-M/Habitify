b4f48d8a2a074562efde579569975cd7c8136b1d
A-m-i-t-M <amitnoreply63@gmail.com>
2025-05-06 20:16:08 +0530

    fixed imports

--- Frontend/components/PrivateRoute.jsx
+++ Frontend/components/PrivateRoute.jsx
@@ -1,8 +1,8 @@
-import { useSelector } from '-redux'
-import { Navigate, Outlet } from '-router-dom';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom';
 
 export default function PrivateRoute() {
     const {currentUser} = useSelector(state => state.user);
     return currentUser ? <Outlet/> : <Navigate to="/signin"/>
 }
-  

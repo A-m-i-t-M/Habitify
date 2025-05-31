8dcd586b905d3bf6040103f8e3bc16db92d0fe2c
A-m-i-t-M <amitnoreply63@gmail.com>
2025-05-31 20:28:03 +0530

    got the missing babel dependencies

--- Frontend/jest.config.js
+++ Frontend/jest.config.js
@@ -3,7 +3,7 @@ export default { // Or module.exports if you're not using ESM here
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Your existing setup file
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Use babel-jest for .js, .jsx, .ts, .tsx files
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // This line tells Jest to use Babel
   },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mocks CSS imports
